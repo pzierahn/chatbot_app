6b3f71a8fabf5c65799935e9f4b45d95dda5795b
patrick <patrick.zierahn@student.kit.edu>
2023-09-11 09:53:41 +0200

    Refactor theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:braingain_app/ui/page/home/home.dart';
+import 'package:braingain_app/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final _borderRadius = BorderRadius.circular(8);
-
-final _roundedShape = RoundedRectangleBorder(
-  borderRadius: _borderRadius,
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,23 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-        chipTheme: ChipThemeData(
-          side: const BorderSide(color: Colors.transparent),
-          shape: _roundedShape,
-        ),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          brightness: Brightness.dark,
-          seedColor: Colors.blue,
-        ),
-        useMaterial3: true,
-      ),
+      theme: lightTheme,
+      darkTheme: darkTheme,
       home: const Home(),
     );
   }
---
+++ lib/ui/theme/theme.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+const _colorSeed = Colors.blue;
+
+final _colorsLight = ColorScheme.fromSeed(
+  brightness: Brightness.light,
+  seedColor: _colorSeed,
+);
+
+final _colorsDark = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: _colorSeed,
+);
+
+final _borderRadius = BorderRadius.circular(16);
+
+final _roundedShape = RoundedRectangleBorder(
+  borderRadius: _borderRadius,
+);
+
+final lightTheme = ThemeData(
+  colorScheme: _colorsLight,
+  chipTheme: ChipThemeData(
+    side: BorderSide(
+      color: _colorsLight.outlineVariant,
+      width: 1.0,
+    ),
+    shape: _roundedShape,
+    backgroundColor: _colorsLight.surfaceVariant.withOpacity(0.25),
+    labelStyle: TextStyle(
+      color: _colorsLight.onSurfaceVariant,
+    ),
+    iconTheme: IconThemeData(
+      color: _colorsLight.onSurfaceVariant,
+    ),
+  ),
+  useMaterial3: true,
+);
+
+final darkTheme = ThemeData(
+  colorScheme: _colorsDark,
+  chipTheme: ChipThemeData(
+    side: BorderSide(
+      color: _colorsDark.outlineVariant,
+      width: 1.0,
+    ),
+    shape: _roundedShape,
+    backgroundColor: _colorsDark.surfaceVariant.withOpacity(0.25),
+    labelStyle: TextStyle(
+      color: _colorsDark.onSurfaceVariant,
+    ),
+    iconTheme: IconThemeData(
+      color: _colorsDark.onSurfaceVariant,
+    ),
+  ),
+  useMaterial3: true,
+);
