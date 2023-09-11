22227940a4f1769f9833b495e9d199b5f745e4d9
patrick <patrick.zierahn@student.kit.edu>
2023-09-11 09:20:06 +0200

    Clean up

--- lib/ui/page/home/home.dart
+++ lib/ui/page/home/home.dart
@@ -13,8 +13,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme;
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -22,21 +20,6 @@ class _HomeState extends State<Home> {
           'Braingain',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.chat,
-              color: color.primary,
-            ),
-            tooltip: 'Chat',
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.description_outlined),
-            tooltip: 'Documents',
-          ),
-        ],
       ),
       body: const Chat(),
     );
