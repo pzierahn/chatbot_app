eca3d5106c7283358a20aca18d6f6f195358ee54
patrick <patrick.zierahn@student.kit.edu>
2023-09-12 16:20:25 +0200

    Added file uploader

--- lib/service/storage.dart
+++ lib/service/storage.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:braingain_app/generated/braingain.pb.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';
@@ -5,19 +8,33 @@ import 'package:uuid/uuid.dart';
 final supabase = Supabase.instance.client;
 
 class StorageUtils {
-  static Future<Object?> upload(PlatformFile file, String collection) async {
+  static StorageRef createRef({
+    required String collection,
+    required PlatformFile file,
+  }) {
     final docId = const Uuid().v4().toString();
     final filePath = "${supabase.auth.currentUser?.id ?? ""}/"
         "$collection/"
         "$docId.pdf";
 
+    return StorageRef()
+      ..id = docId
+      ..filename = file.name
+      ..path = filePath
+      ..collection = collection;
+  }
+
+  static Future<StorageRef> upload(
+    StorageRef ref,
+    Uint8List bytes,
+  ) async {
     final bucket = supabase.storage.from('documents');
 
     await bucket.uploadBinary(
-      filePath,
-      file.bytes!,
+      ref.path,
+      bytes,
     );
 
-    return null;
+    return ref;
   }
 }
--- lib/ui/page/collection/collection_page.dart
+++ lib/ui/page/collection/collection_page.dart
@@ -1,14 +1,19 @@
 import 'dart:math';
 
+import 'package:braingain_app/generated/braingain.pb.dart';
+import 'package:braingain_app/service/braingain.dart';
 import 'package:braingain_app/service/storage.dart';
 import 'package:braingain_app/ui/widget/constrained_list_view.dart';
 import 'package:braingain_app/utils/login.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:grpc/grpc.dart';
 
 class CollectionPage extends StatefulWidget {
-  const CollectionPage(
-      {super.key, this.collection = "b452f76d-c1e4-4cdb-979f-08a4521d3372"});
+  const CollectionPage({
+    super.key,
+    this.collection = "b452f76d-c1e4-4cdb-979f-08a4521d3372",
+  });
 
   final String collection;
 
@@ -25,32 +30,51 @@ class CollectionPage extends StatefulWidget {
 }
 
 class _CollectionPageState extends State<CollectionPage> {
-  static const _documents = <String>[
-    'DeSys_01_Intro',
-    'DeSys_02_Overlay_P2P_Sybil',
-    'DeSys_03_Fomalization',
-    'DeSys_04_Leader_Election',
-    'DeSys_05_Mutual_Exclusion',
-    'DeSys_06_Byzantine_Reliable_Broadcast',
-    'DeSys_07_Consensus_and_Variants_v2',
-    'DeSys_07_Current_Research_Consensus_and_Variants',
-    'DeSys_08_Consistency',
-    'DeSys_09_Decentralized_Messaging_Matrix',
-    'DeSys_10_Distributed_Ledgers_Blockchains_Bitcoin',
-    'DeSys_11_Payment_Channel_Networks',
-    'DeSys_12_Smart_Contract_Platforms_Ethereum',
-    'DeSys_13_Decentralized_File_Storage_IPFS',
-    'DeSys_14_Sociotechnical_Conclusion',
-  ];
+  final _queue = <StorageRef>[];
+  final _progress = <String, ResponseStream<IndexProgress>>{};
+
+  Future<void> _uploadFiles() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowMultiple: true,
+      allowedExtensions: ['pdf'],
+      withData: true,
+    );
+
+    if (result == null) {
+      return;
+    }
+
+    for (final file in result.files) {
+      final ref = StorageUtils.createRef(
+        collection: widget.collection,
+        file: file,
+      );
+
+      debugPrint('file: ${file.name} ${file.bytes?.length}');
+
+      StorageUtils.upload(ref, file.bytes!).then((value) {
+        debugPrint('uploaded: ${value.filename}');
+
+        setState(() {
+          _queue.add(ref);
+          _progress[ref.id] = braingain.indexDocument(ref);
+        });
+      }).catchError((error, stackTrace) {
+        debugPrint('error: $error');
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).colorScheme;
-    final text = Theme.of(context).textTheme;
+
+    _queue.sort((a, b) => a.filename.compareTo(b.filename));
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Collection'),
+        title: const Text('Upload'),
         actions: [
           IconButton(
             onPressed: () {
@@ -60,59 +84,34 @@ class _CollectionPageState extends State<CollectionPage> {
           ),
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () async {
-              final result = await FilePicker.platform.pickFiles(
-                type: FileType.custom,
-                allowMultiple: true,
-                allowedExtensions: ['pdf'],
-                withData: true,
-              );
-
-              if (result != null) {
-                for (final file in result.files) {
-                  debugPrint('file: ${file.name} ${file.bytes?.length}');
-                  StorageUtils.upload(file, widget.collection).onError(
-                    (error, stackTrace) {
-                      debugPrint('error: $error');
-                      return null;
-                    },
-                  );
-                }
-              }
-            },
+            onPressed: _uploadFiles,
           ),
         ],
       ),
       body: ConstrainedListView(
-          children: _documents
+          children: _queue
               .map(
-                (title) => ListTile(
-                  leading: const Icon(Icons.description_outlined),
-                  title: Text(
-                    title,
-                    style: text.titleMedium,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text(
-                    '${Random().nextInt(100)} Pages',
-                    style: text.bodySmall?.merge(
-                      TextStyle(
-                        color: color.secondary,
+                (ref) => StreamBuilder<IndexProgress>(
+                  stream: _progress[ref.id],
+                  builder: (context, stream) {
+                    final progress = stream.hasData
+                        ? (stream.data!.processedPages /
+                            stream.data!.totalPages)
+                        : 0.0;
+
+                    debugPrint('stream: ${progress.toStringAsFixed(2)}');
+
+                    return ListTile(
+                      leading: Icon(
+                        Icons.upload,
+                        color: color.primary,
                       ),
-                    ),
-                  ),
-                  onTap: () {},
-                  trailing: PopupMenuButton(
-                    itemBuilder: (context) => const [
-                      PopupMenuItem(
-                        child: Text('Rename'),
+                      title: Text(ref.filename),
+                      subtitle: LinearProgressIndicator(
+                        value: progress,
                       ),
-                      PopupMenuItem(
-                        child: Text('Delete'),
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               )
               .toList()),
