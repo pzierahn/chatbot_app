6955fa86ccc49abaef2a77d65203d4f19a8262e9
patrick <patrick.zierahn@student.kit.edu>
2023-09-19 23:52:17 +0200

    Show illustration when collection is empty

--- lib/ui/page/collection/collection_page.dart
+++ lib/ui/page/collection/collection_page.dart
@@ -2,7 +2,9 @@ import 'package:braingain_app/generated/braingain.pb.dart';
 import 'package:braingain_app/service/braingain.dart';
 import 'package:braingain_app/ui/page/upload/upload_page.dart';
 import 'package:braingain_app/ui/widget/constrained_list_view.dart';
+import 'package:braingain_app/ui/widget/illustration.dart';
 import 'package:flutter/material.dart';
+import 'package:undraw/illustrations.g.dart';
 
 class CollectionPage extends StatefulWidget {
   const CollectionPage({
@@ -30,6 +32,8 @@ class CollectionPage extends StatefulWidget {
 }
 
 class _CollectionPageState extends State<CollectionPage> {
+  void _onUpload() => UploadPage.open(context, widget.collection);
+
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).colorScheme;
@@ -41,9 +45,7 @@ class _CollectionPageState extends State<CollectionPage> {
         actions: [
           IconButton(
             icon: const Icon(Icons.upload),
-            onPressed: () {
-              UploadPage.open(context, widget.collection);
-            },
+            onPressed: _onUpload,
           ),
         ],
       ),
@@ -55,6 +57,20 @@ class _CollectionPageState extends State<CollectionPage> {
           if (!snap.hasData) {
             return const Center(
               child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snap.data!.items.isEmpty) {
+            return Center(
+              child: TextIllustration(
+                width: 300,
+                illustration: UnDrawIllustration.empty,
+                action: TextButton.icon(
+                  onPressed: _onUpload,
+                  icon: const Icon(Icons.upload),
+                  label: const Text('Upload Document'),
+                ),
+              ),
             );
           }
 
