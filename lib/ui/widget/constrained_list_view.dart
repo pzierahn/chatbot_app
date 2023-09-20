8d2bde0a252be86bf8ad6925043b2f5ba79db318
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 14:16:32 +0200

    Added ConstrainedListViewBuilder

--- lib/ui/widget/constrained_list_view.dart
+++ lib/ui/widget/constrained_list_view.dart
@@ -16,20 +16,51 @@ class ConstrainedListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ConstrainedListViewBuilder(
+      itemCount: children.length,
+      itemBuilder: (context, index) {
+        return children[index];
+      },
+      maxWidth: maxWidth,
       shrinkWrap: shrinkWrap,
       primary: primary,
-      children: children
-          .map((child) => Center(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  constraints: BoxConstraints(
-                    maxWidth: maxWidth,
-                  ),
-                  child: child,
-                ),
-              ))
-          .toList(),
+    );
+  }
+}
+
+class ConstrainedListViewBuilder extends StatelessWidget {
+  const ConstrainedListViewBuilder({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    this.maxWidth = 800,
+    this.shrinkWrap = false,
+    this.primary,
+  });
+
+  final int itemCount;
+  final NullableIndexedWidgetBuilder itemBuilder;
+  final double maxWidth;
+  final bool shrinkWrap;
+  final bool? primary;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: shrinkWrap,
+      primary: primary,
+      itemCount: itemCount,
+      itemBuilder: (context, index) {
+        return Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            constraints: BoxConstraints(
+              maxWidth: maxWidth,
+            ),
+            child: itemBuilder(context, index),
+          ),
+        );
+      },
     );
   }
 }
