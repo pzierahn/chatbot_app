13b94db8dae6d01e675acbe856d0a4758de00513
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 15:57:53 +0200

    Added ConfirmDialog to logout

--- lib/ui/page/settings/settings_page.dart
+++ lib/ui/page/settings/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:braingain_app/service/supabase.dart';
+import 'package:braingain_app/ui/widget/confirm_dialog.dart';
 import 'package:braingain_app/ui/widget/constrained_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -18,8 +19,20 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  void _onLogout(BuildContext context) async {
+    ConfirmDialog.show(
+      context,
+      title: 'Logout',
+      content: 'Are you sure?',
+      onConfirm: () => supabase.auth.signOut(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    final text = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
@@ -27,11 +40,19 @@ class _SettingsPageState extends State<SettingsPage> {
       body: ConstrainedListView(
         children: [
           ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('Logout'),
-            onTap: () {
-              supabase.auth.signOut();
-            },
+            leading: const Icon(Icons.person_outline),
+            title: Text('User ID', style: text.titleMedium),
+            subtitle: SelectableText(
+              supabase.auth.currentUser?.id ?? 'Not logged in',
+            ),
+            trailing: TextButton.icon(
+              onPressed: () => _onLogout(context),
+              icon: const Icon(Icons.logout),
+              style: IconButton.styleFrom(
+                foregroundColor: color.error,
+              ),
+              label: const Text('Logout'),
+            ),
           ),
         ],
       ),
