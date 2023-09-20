b61c19025f47eb86083b45c40c8203582d32bb8b
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 19:16:21 +0200

    Use ClientInterceptor to add auth token to request

--- lib/service/braingain.dart
+++ lib/service/braingain.dart
@@ -1,6 +1,6 @@
 import 'package:braingain_app/generated/braingain.pbgrpc.dart';
-import 'package:braingain_app/generated/google/protobuf/empty.pb.dart';
 import 'package:braingain_app/service/supabase.dart';
+import 'package:grpc/grpc.dart';
 import 'package:grpc/grpc_or_grpcweb.dart';
 
 final _channel = GrpcOrGrpcWebClientChannel.toSeparateEndpoints(
@@ -12,12 +12,12 @@ final _channel = GrpcOrGrpcWebClientChannel.toSeparateEndpoints(
   grpcWebTransportSecure: false,
 );
 
-final braingain = BraingainClientWithToken(_channel);
+final braingain = BraingainClient(_channel, interceptors: [
+  _AuthenticationInterceptor(),
+]);
 
-class BraingainClientWithToken extends BraingainClient {
-  BraingainClientWithToken(super.channel);
-
-  CallOptions? _mergeAuth(CallOptions? options) {
+class _AuthenticationInterceptor implements ClientInterceptor {
+  CallOptions _mergeAuth(CallOptions options) {
     final token = supabase.auth.currentSession?.accessToken;
 
     if (token == null) {
@@ -32,109 +32,23 @@ class BraingainClientWithToken extends BraingainClient {
   }
 
   @override
-  ResponseFuture<ChatMessage> chat(Prompt request, {CallOptions? options}) {
-    return super.chat(
-      request,
-      options: _mergeAuth(options),
-    );
+  ResponseFuture<R> interceptUnary<Q, R>(
+    ClientMethod<Q, R> method,
+    Q request,
+    CallOptions options,
+    invoker,
+  ) {
+    options = _mergeAuth(options);
+    return invoker(method, request, options);
   }
 
   @override
-  ResponseFuture<ChatMessages> getChatMessages(Collection request,
-      {CallOptions? options}) {
-    return super.getChatMessages(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<ChatMessage> getChatMessage(MessageID request,
-      {CallOptions? options}) {
-    return super.getChatMessage(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Documents> listDocuments(DocumentFilter request,
-      {CallOptions? options}) {
-    return super.listDocuments(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Empty> deleteDocument(Document request,
-      {CallOptions? options}) {
-    return super.deleteDocument(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Empty> updateDocument(Document request,
-      {CallOptions? options}) {
-    return super.updateDocument(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Collections> getCollections(Empty request,
-      {CallOptions? options}) {
-    return super.getCollections(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Empty> createCollection(Collection request,
-      {CallOptions? options}) {
-    return super.createCollection(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Empty> deleteCollection(Collection request,
-      {CallOptions? options}) {
-    return super.deleteCollection(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<Empty> updateCollection(Collection request,
-      {CallOptions? options}) {
-    return super.updateCollection(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseStream<IndexProgress> indexDocument(Document request,
-      {CallOptions? options}) {
-    return super.indexDocument(
-      request,
-      options: _mergeAuth(options),
-    );
-  }
-
-  @override
-  ResponseFuture<ModelUsages> getModelUsages(Empty request,
-      {CallOptions? options}) {
-    return super.getModelUsages(
-      request,
-      options: _mergeAuth(options),
-    );
+  ResponseStream<R> interceptStreaming<Q, R>(
+      ClientMethod<Q, R> method,
+      Stream<Q> requests,
+      CallOptions options,
+      ClientStreamingInvoker<Q, R> invoker) {
+    options = _mergeAuth(options);
+    return invoker(method, requests, options);
   }
 }
