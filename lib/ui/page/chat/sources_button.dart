27fe830af5c60f7919415ea90731051a214da551
patrick <patrick.zierahn@student.kit.edu>
2023-09-23 00:13:18 +0200

    Show document count

--- lib/ui/page/chat/sources_button.dart
+++ lib/ui/page/chat/sources_button.dart
@@ -13,11 +13,25 @@ class SourcesButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final text = Theme.of(context).textTheme;
+    final color = Theme.of(context).colorScheme;
+
+    final count = documents.length;
+    final name = count == 1 ? 'Source' : 'Sources';
 
     return ActionChip(
-      avatar: const Icon(Icons.find_in_page_outlined, size: 16),
+      avatar: CircleAvatar(
+        backgroundColor: color.surfaceVariant,
+        foregroundColor: color.onSurfaceVariant,
+        child: Text(
+          count.toString(),
+          style: text.bodySmall?.copyWith(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       label: Text(
-        'Sources',
+        name,
         style: text.bodySmall,
       ),
       onPressed: () => SourcesDialog.show(
