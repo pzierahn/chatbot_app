79e2f189745d267bafa0379c80ac52d08f70e0fd
patrick <patrick.zierahn@student.kit.edu>
2023-09-22 14:37:58 +0200

    Update title

--- lib/ui/page/home/home.dart
+++ lib/ui/page/home/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Braingain',
+          'Brainboost',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         actions: [
