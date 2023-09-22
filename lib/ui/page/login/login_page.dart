56cbcc7a62342adb3b88da8adad2a6341282faea
patrick <patrick.zierahn@student.kit.edu>
2023-09-22 22:56:28 +0200

    Added scroll view to login

--- lib/ui/page/login/login_page.dart
+++ lib/ui/page/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:braingain_app/service/supabase.dart';
+import 'package:braingain_app/ui/widget/constrained_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:undraw/undraw.dart';
 
@@ -29,62 +30,71 @@ class _LoginPageState extends State<LoginPage> {
     final color = Theme.of(context).colorScheme;
     final text = Theme.of(context).textTheme;
 
+    const prefixConstraints = BoxConstraints.tightFor(width: 56);
+
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: 300,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Braingain',
-                style: text.displaySmall?.merge(
-                  const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 16),
-              UnDraw(
-                illustration: UnDrawIllustration.forgot_password,
-                // illustration: UnDrawIllustration.fingerprint,
-                color: color.primary,
-                width: 200,
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: userController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  hintText: 'Type your email',
-                  prefixIcon: Icon(Icons.person_outline),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Type your password',
-                  prefixIcon: Icon(Icons.lock_outline),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 16),
-              FilledButton(
-                onPressed: () async {
-                  final email = userController.text;
-                  final password = passwordController.text;
-                  await supabase.auth.signInWithPassword(
-                    email: email,
-                    password: password,
-                  );
-                },
-                child: const Text('Login'),
-              ),
-            ],
+      body: ConstrainedListView(
+        padding: const EdgeInsets.all(8),
+        maxWidth: 500,
+        children: [
+          const SizedBox(height: 100),
+          Text(
+            'Braingain',
+            style: text.displaySmall?.merge(
+              const TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
+          const SizedBox(height: 16),
+          UnDraw(
+            illustration: UnDrawIllustration.forgot_password,
+            // illustration: UnDrawIllustration.fingerprint,
+            color: color.primary,
+            width: 200,
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: userController,
+            autofillHints: const [
+              AutofillHints.email,
+            ],
+            decoration: const InputDecoration(
+              labelText: 'Username',
+              hintText: 'Type your email',
+              prefixIcon: Icon(Icons.person_outline),
+              prefixIconConstraints: prefixConstraints,
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: passwordController,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            autofillHints: const [
+              AutofillHints.password,
+            ],
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              hintText: 'Type your password',
+              prefixIcon: Icon(Icons.lock_outline),
+              prefixIconConstraints: prefixConstraints,
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          FilledButton(
+            onPressed: () async {
+              final email = userController.text;
+              final password = passwordController.text;
+              await supabase.auth.signInWithPassword(
+                email: email,
+                password: password,
+              );
+            },
+            child: const Text('Login'),
+          ),
+        ],
       ),
     );
   }
