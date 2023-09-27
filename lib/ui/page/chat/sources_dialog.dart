ab8d2ecd44138e23c6e23a447ce7beb6b2e29b62
patrick <patrick.zierahn@student.kit.edu>
2023-09-27 16:23:25 +0200

    Remove dead code

--- lib/ui/page/chat/sources_dialog.dart
+++ lib/ui/page/chat/sources_dialog.dart
@@ -1,7 +1,5 @@
 import 'package:braingain_app/generated/chat.pb.dart';
-import 'package:braingain_app/generated/documents.pb.dart';
 import 'package:braingain_app/ui/widget/illustration.dart';
-import 'package:braingain_app/utils/page_numbers.dart';
 import 'package:flutter/material.dart';
 import 'package:undraw/illustrations.g.dart';
 
@@ -112,99 +110,6 @@ class _SourcesDialogState extends State<SourcesDialog> {
           child: const Text('Close'),
         ),
       ],
-    );
-  }
-}
-
-class _DocumentsBody extends StatefulWidget {
-  const _DocumentsBody({
-    required this.documents,
-    required this.selected,
-    required this.onChanged,
-  });
-
-  final Documents documents;
-  final Map<String, List<int>> selected;
-  final ValueChanged<Map<String, List<int>>> onChanged;
-
-  @override
-  State<_DocumentsBody> createState() => _DocumentsBodyState();
-}
-
-class _DocumentsBodyState extends State<_DocumentsBody> {
-  @override
-  Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme;
-    final text = Theme.of(context).textTheme;
-
-    return Column(
-      children: widget.documents.items
-          .map((doc) => ListTile(
-                leading: widget.selected.containsKey(doc.id)
-                    ? Icon(
-                        Icons.check_circle,
-                        size: 16,
-                        color: color.primary,
-                      )
-                    : const Icon(Icons.description, size: 16),
-                title: Text(
-                  doc.filename,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: text.bodySmall?.merge(TextStyle(
-                    color: widget.selected.containsKey(doc.id)
-                        ? color.primary
-                        : color.onSurface,
-                  )),
-                ),
-                subtitle: widget.selected.containsKey(doc.id)
-                    ? TextFormField(
-                        controller: TextEditingController(
-                          text: formatPageList(widget.selected[doc.id]!),
-                        ),
-                        decoration: const InputDecoration.collapsed(
-                          hintText: 'Pages',
-                        ),
-                        style: text.bodySmall?.merge(TextStyle(
-                          color: color.outline,
-                        )),
-                        onFieldSubmitted: (text) {
-                          widget.selected[doc.id] = parsePageList(text);
-                          widget.onChanged(widget.selected);
-                        },
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter page numbers';
-                          }
-
-                          final reg =
-                              RegExp(r'^\d+(-\d+)?,?( *\d+(-\d+)?,?)*$');
-                          if (!reg.hasMatch(value)) {
-                            return 'Please enter page numbers correctly';
-                          }
-
-                          return null;
-                        },
-                      )
-                    : Text(
-                        'Pages: ${doc.pages}',
-                        style: text.bodySmall?.merge(TextStyle(
-                          color: color.outline,
-                        )),
-                      ),
-                onTap: () {
-                  if (widget.selected.containsKey(doc.id)) {
-                    widget.selected.remove(doc.id);
-                  } else {
-                    widget.selected[doc.id] = [
-                      for (int i = 1; i <= doc.pages; i++) i
-                    ];
-                  }
-
-                  widget.onChanged(widget.selected);
-                },
-              ))
-          .toList(),
     );
   }
 }
