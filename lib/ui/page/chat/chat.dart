5f219de0c1fa9c4d6d6a80589eea785f9c29e54b
patrick <patrick.zierahn@student.kit.edu>
2023-10-03 19:53:39 +0200

    Added chat header text and button

--- lib/ui/page/chat/chat.dart
+++ lib/ui/page/chat/chat.dart
@@ -2,6 +2,9 @@ import 'package:braingain_app/generated/collections.pb.dart';
 import 'package:braingain_app/service/brainboost.dart';
 import 'package:braingain_app/ui/page/chat/chat_fragment.dart';
 import 'package:braingain_app/ui/page/chat/chat_fragment_input.dart';
+import 'package:braingain_app/ui/page/chat_history/chat_history_page.dart';
+import 'package:braingain_app/ui/page/collection/collection_page.dart';
+import 'package:braingain_app/ui/page/upload/upload_page.dart';
 import 'package:braingain_app/ui/widget/constrained_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -22,7 +25,69 @@ class _ChatState extends State<Chat> {
 
   @override
   Widget build(BuildContext context) {
-    final children = <Widget>[];
+    final color = Theme.of(context).colorScheme;
+
+    final children = <Widget>[
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const SizedBox(height: 24),
+            Text(
+              widget.collection.name,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Type your question to learn more about ${widget.collection.name}\n',
+              style: Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              alignment: WrapAlignment.center,
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    ChatHistoryPage.open(context, widget.collection);
+                  },
+                  icon: const Icon(Icons.history),
+                  label: const Text('History'),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    CollectionPage.open(context, widget.collection);
+                  },
+                  icon: CircleAvatar(
+                    backgroundColor: color.primary,
+                    foregroundColor: color.onPrimary,
+                    radius: 8,
+                    child: Text(
+                      '${widget.collection.documentCount}',
+                      style: const TextStyle(
+                        fontSize: 8,
+                      ),
+                    ),
+                  ),
+                  label: const Text('Documents'),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    UploadPage.openWithDialog(context, widget.collection);
+                  },
+                  icon: const Icon(Icons.upload_file),
+                  label: const Text('Upload'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    ];
 
     for (int index = 0; index < _status.length; index++) {
       final fragment = ChatFragment(
