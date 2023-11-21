ccc53f109c5db335a54299c89071815d01dbb1e7
patrick <patrick.zierahn@student.kit.edu>
2023-11-21 21:09:12 +0100

    Update version

--- lib/ui/page/settings/settings_page.dart
+++ lib/ui/page/settings/settings_page.dart
@@ -3,7 +3,7 @@ import 'package:braingain_app/ui/page/settings/balance.dart';
 import 'package:braingain_app/ui/widget/constrained_list_view.dart';
 import 'package:flutter/material.dart';
 
-const _version = '1.0.6';
+const _version = '1.0.7';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({
