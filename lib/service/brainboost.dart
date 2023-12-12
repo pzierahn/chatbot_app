7601d99fbda1a6aabe1ad4ffa8514e1fa69bda8e
patrick <patrick.zierahn@student.kit.edu>
2023-12-12 20:45:40 +0100

    Added GCP enpoints

--- lib/service/brainboost.dart
+++ lib/service/brainboost.dart
@@ -7,22 +7,22 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:grpc/grpc.dart';
 import 'package:grpc/grpc_or_grpcweb.dart';
 
-final _channel = ClientChannel(
-  'localhost',
-  port: 9055,
-  options: const ChannelOptions(
-    credentials: ChannelCredentials.insecure(),
-  ),
-);
-
-// final _channel = GrpcOrGrpcWebClientChannel.toSeparateEndpoints(
-//   grpcHost: 'brainboost-service-2qkjmuus4a-ey.a.run.app',
-//   grpcPort: 443,
-//   grpcTransportSecure: true,
-//   grpcWebHost: 'brainboost-gateway-2qkjmuus4a-ey.a.run.app',
-//   grpcWebPort: 443,
-//   grpcWebTransportSecure: true,
+// final _channel = ClientChannel(
+//   'localhost',
+//   port: 9055,
+//   options: const ChannelOptions(
+//     credentials: ChannelCredentials.insecure(),
+//   ),
 // );
+
+final _channel = GrpcOrGrpcWebClientChannel.toSeparateEndpoints(
+  grpcHost: 'brainboost-services-2qkjmuus4a-ey.a.run.app',
+  grpcPort: 443,
+  grpcTransportSecure: true,
+  grpcWebHost: 'brainboost-gateway-v2-2qkjmuus4a-ey.a.run.app',
+  grpcWebPort: 443,
+  grpcWebTransportSecure: true,
+);
 
 final collections = CollectionServiceClientAuth();
 
