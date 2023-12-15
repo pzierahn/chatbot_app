3e5be9496a9f208ed9ef860745015c891f35ddff
patrick <patrick.zierahn@student.kit.edu>
2023-12-15 20:26:54 +0100

    Remove imports

--- lib/service/storage.dart
+++ lib/service/storage.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:braingain_app/generated/documents.pb.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
