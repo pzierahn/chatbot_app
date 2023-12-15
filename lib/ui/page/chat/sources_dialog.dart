3248ee5fd078b0b79a4dd7dec755488a5e447479
patrick <patrick.zierahn@student.kit.edu>
2023-12-15 20:32:02 +0100

    Load and show sources

--- lib/ui/page/chat/sources_dialog.dart
+++ lib/ui/page/chat/sources_dialog.dart
@@ -1,3 +1,5 @@
+import 'package:braingain_app/generated/documents.pbgrpc.dart';
+import 'package:braingain_app/service/brainboost.dart';
 import 'package:braingain_app/ui/widget/illustration.dart';
 import 'package:flutter/material.dart';
 import 'package:undraw/illustrations.g.dart';
@@ -24,29 +26,80 @@ class _SourcesDialogState extends State<SourcesDialog> {
 
     final sources = widget.references;
 
-    // TODO: Fetch document names from server
-
     Widget body;
 
     if (sources.isNotEmpty) {
-      body = ListView.separated(
-        itemCount: sources.length,
-        separatorBuilder: (context, index) => const Divider(height: 32),
-        itemBuilder: (context, index) {
-          final source = widget.references[index];
+      body = FutureBuilder<References>(
+        future: documents.getReferences(
+          ReferenceIDs()..items.addAll(sources),
+        ),
+        builder: (context, snap) {
+          if (snap.hasError) {
+            return Center(
+              child: TextIllustration(
+                illustration: UnDrawIllustration.by_the_road,
+                color: color.primary,
+                text: 'Failed to load sources',
+              ),
+            );
+          }
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SelectableText(
-                  source,
-                  style: text.titleSmall,
-                  maxLines: 1,
+          if (!snap.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final references = snap.data!.items;
+
+          final files = references.map((e) => e.filename).toSet().toList();
+          files.sort();
+
+          final filePages = <String, List<int>>{};
+
+          for (final ref in references) {
+            final file = ref.filename;
+            filePages.putIfAbsent(file, () => []);
+            filePages[file]?.add(ref.page);
+          }
+
+          return ListView.separated(
+            itemCount: files.length,
+            separatorBuilder: (context, index) => const Divider(height: 32),
+            itemBuilder: (context, index) {
+              final file = files[index];
+              final pages = filePages[file] ?? [];
+              pages.sort();
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SelectableText(
+                      file,
+                      style: text.titleSmall,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 8),
+                    Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      children: pages
+                          .map(
+                            (e) => Chip(
+                              label: Text(
+                                'Page $e',
+                                style: text.caption,
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              );
+            },
           );
         },
       );
