e032a52c70d58ce3373f562f8ef63b54245d8218
patrick <patrick.zierahn@student.kit.edu>
2024-02-13 00:53:19 +0100

    Clean up

--- lib/service/brainboost.dart
+++ lib/service/brainboost.dart
@@ -4,7 +4,6 @@ import 'package:braingain_app/generated/collection_service.pbgrpc.dart';
 import 'package:braingain_app/generated/document_service.pbgrpc.dart';
 import 'package:braingain_app/generated/google/protobuf/empty.pb.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:grpc/grpc.dart';
 import 'package:grpc/grpc_or_grpcweb.dart';
 
 // final _channel = GrpcOrGrpcWebClientChannel.grpc(
