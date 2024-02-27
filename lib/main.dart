60cff38c17d72e06f6067227254c140d8d39feac
patrick <patrick.zierahn@student.kit.edu>
2024-02-27 11:55:39 +0100

    Implement crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:braingain_app/generated/google/protobuf/empty.pb.dart';
+import 'package:braingain_app/service/brainboost.dart';
+import 'package:braingain_app/generated/crashlytics.pbgrpc.dart';
 import 'package:braingain_app/ui/page/chat/chat_page.dart';
 import 'package:braingain_app/ui/page/chat_history/chat_history_page.dart';
 import 'package:braingain_app/ui/page/documents/documents_page.dart';
@@ -9,6 +12,7 @@ import 'package:braingain_app/ui/page/index/index_page.dart';
 import 'package:braingain_app/ui/theme/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -20,6 +24,30 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Non-async exceptions
+  FlutterError.onError = (errorDetails) {
+    final err = Error()
+      ..exception = errorDetails.exception.toString()
+      ..stackTrace = errorDetails.stack.toString()
+      ..appVersion = appVersion;
+
+    // Catch error to prevent infinite loop
+    crashlytics.recordError(err).catchError((_) => Empty());
+  };
+
+  // Async exceptions
+  PlatformDispatcher.instance.onError = (error, stack) {
+    final err = Error()
+      ..exception = error.toString()
+      ..stackTrace = stack.toString()
+      ..appVersion = appVersion;
+
+    // Catch error to prevent infinite loop
+    crashlytics.recordError(err).catchError((_) => Empty());
+    return true;
+  };
+
   runApp(const App());
 }
 
