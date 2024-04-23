d40f9a32f40429a918884fbfbf87ca81fce7c592
patrick <patrick.zierahn@student.kit.edu>
2024-04-23 13:45:27 +0200

    Added GeneratingText widget

---
+++ lib/ui/page/chat/generating_text.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class GeneratingText extends StatefulWidget {
+  const GeneratingText({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _GeneratingTextState();
+}
+
+class _GeneratingTextState extends State<GeneratingText> {
+  late Timer _timer;
+
+  int _points = 4;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 200),
+      (Timer timer) {
+        setState(() {
+          _points = (_points + 1) % 4;
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String _text = 'Generating';
+
+    for (int inx = 0; inx < _points; inx++) {
+      _text += '.';
+    }
+
+    return Text(_text);
+  }
+}
