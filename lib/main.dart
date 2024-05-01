01d1dc95f0ab1e55af75687bb8e91f9da1570e4e
patrick <patrick.zierahn@student.kit.edu>
2024-05-01 09:50:32 +0200

    Update version

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
-const appVersion = 'v3.12.4';
+const appVersion = 'v3.13.0';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
