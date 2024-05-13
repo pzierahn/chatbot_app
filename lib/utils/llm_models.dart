220cfd3e912d0c1cc6d1c5a8bed0ded7b8f5ed50
patrick <patrick.zierahn@student.kit.edu>
2024-05-13 22:36:06 +0200

    Added GPT 4o

--- lib/utils/llm_models.dart
+++ lib/utils/llm_models.dart
@@ -12,8 +12,8 @@ class LLMModel {
 
 class LLMModels {
   static const LLMModel gpt4 = LLMModel(
-    title: 'GPT 4 Turbo',
-    model: 'openai.gpt-4-turbo',
+    title: 'GPT 4o',
+    model: 'openai.gpt-4o',
     provider: 'OpenAI',
   );
 
