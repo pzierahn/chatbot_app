745bf5664bbda37d7870a91e2f0f20f2437c57e2
patrick <patrick.zierahn@student.kit.edu>
2024-07-12 15:14:38 +0200

    Version 4.0.4 not found

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
-const appVersion = 'v4.0.3';
+const appVersion = 'v4.0.4';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
