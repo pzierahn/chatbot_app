d75f769a2139fc298fd513d5f783ad8f2d5af99c
Patrick Zierahn <patrick.zierahn@gmail.com>
2025-02-17 12:28:10 +0100

    Update version to 4.0.10

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
-const appVersion = 'v4.0.8';
+const appVersion = 'v4.0.10';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
