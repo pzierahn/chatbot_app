7a59713fea9e3e22e8da2faab74a864e2819e4e5
Patrick Zierahn <patrick.zierahn@gmail.com>
2025-04-08 11:15:07 +0200

    Bump version to 4.3.0 and add model selection for Notion

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
-const appVersion = 'v4.2.0';
+const appVersion = 'v4.3.0';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
